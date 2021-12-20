0ecd159f032d7dc7dc5aeb99b379a3118af00488
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-20 13:00:27 +0600

    db conneciton setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('../config/db');
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
