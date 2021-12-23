176bd0ef3cc5771eba5cec2966fe8d823c3965fa
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-23 12:53:29 +0600

    product showing

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
-const Product = () => {
+
+const Product = ({ imageUrl, description, price, name, productId }) => {
     return (
         <Col md={4}>
             <div>
-                <img src="" alt="" />
-                <h2>Title</h2>
-                <p>Description</p>
-                <h6>Price</h6>
-                <button>details</button>
+                <img className="img-fluid" src={imageUrl} alt={name} />
+
+                <div >
+                    <p className="">{name}</p>
+
+                    <p className="">{description.substring(0, 100)}...</p>
+
+                    <p className="">${price}</p>
+
+                    <Link to={`/product/${productId}`} className="">
+                        View
+                    </Link>
+                </div>
             </div>
         </Col>
     );
